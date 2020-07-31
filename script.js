545f6b1f7e411daa00f217ab05ab293d66070de2
G-glo <glochela95@gmail.com>
2020-07-31 16:07:36 +0300

    this is my first commit

---
+++ script.js
@@ -0,0 +1,34 @@
+Constant button:document.querySelector
+button.array.forEach(element => {
+    function add(x,y){
+        return x+y;
+    }
+    function substract(x,y) {
+        return x-y;
+    }
+    function multiply(*){
+        return x*y;
+    }
+    function divide (/){
+        return x/y;
+    }
+})
+function operation(x,operator,y)
+{ switch (operator){
+    case'+'
+    return add(x,y)
+    case '-'
+    return substract(x,y)
+    case 'x'
+    return multiply(x,y)
+    case '/'
+    retuen divide(x/y)
+}}
+function operation button.for forEach
+(operation)
+if (operation.id= reset)[
+    operation.onclick=>{
+        operating=truegit
+        
+    }
+]
